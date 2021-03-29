ffdac70d4ea63df7c02b2b141c3507d07aea8d82
suzukiryoji <suzukiryoji@localhost>
2021-03-29 18:36:19 +0900

    answers

---
+++ src/main.java
@@ -0,0 +1,16 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		int[] numbers = {3, 4, 9};
+		System.out.println("１桁の数字を入力してください");
+		int input = new java.util.Scanner(System.in).nextInt();
+		for (int value:numbers) {
+			if (value == input) {
+				System.out.println("アタリ！");
+			}
+		}
+
+	}
+
+}
